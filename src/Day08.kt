4d97df8d5a7607df915c37a81648df2afc62fa47
bouceka <adamm01b@gmail.com>
2022-10-30 15:09:38 -0700

    refactor: Refactor code

--- src/Day08.kt
+++ src/Day08.kt
@@ -1,32 +1,30 @@
 fun main() {
 	fun part1(input: List<String>) {
-		var lines = List(50) { '.' } // initialize the lines
-		var grid = List(6) { lines.toMutableList() } // thank Ash for this heck ;-)
-		grid.toMutableList()
+		val lines = List(50) { '.' } // initialize the lines
+		val grid = List(6) { lines.toMutableList() }.toMutableList() // thank Ash for this heck ;-)
 
 		input.forEach { it -> // iterate the input
-			var line = it.trim(); // remove potential spaces around the string
+			val line = it.trim() // remove potential spaces around the string
 			val words = line.split(" ") // make a list of instructions per line
 			if (words.first() == "rect") { // get the first operation
-				var (rangeY, rangeX) = words[1].split("x") // destructing dimension
+				val (rangeY, rangeX) = words[1].split("x") // destructing dimension
 				for (y in 0 until rangeY.toInt() ) {
 					for (x in 0 until rangeX.toInt() ) {
 						grid[x][y] = '#' // add lit pixels
 					}
 				}
 			} else if (words.first() == "rotate") {
-				var (directinon, ln) = words[2].split("=") // destruct direction X or Y and line could be horizontal or vertical
+				val (direction, ln) = words[2].split("=") // destruct direction X or Y and line could be horizontal or vertical
 				val line = ln.toInt() // parse into Int
-				var by = words[4].toInt() // number of pixels we have to shift the line
-				if (directinon == "x") { // column
-					var column = List(6) { grid[it][line] } // save a column to change
+				val by = words.last().toInt() // number of pixels we have to shift the line
+				if (direction == "x") { // column
+					val column = List(6) { grid[it][line] } // save a column to change
 					for (x in 0 until 6) {
 						grid[x][line] = column[(x - by + 6) % 6] // iterate through the saved array and shift by the modulo
 					}
-				} else if (directinon == "y") { // row
-					var row = List(50) { grid[line][it] }
+				} else if (direction == "y") { // row
+					val row = List(50) { grid[line][it] }
 					for (y in 0 until 50) {
-					println((y - by + 50) % 50)
 						grid[line][y] = row[(y - by + 50) % 50]
 					}
 				}
